0600ca5fe30583a6059801a6f26b0eae1aae1b3d
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-10 18:41:06 -0300

    init structure project

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routes = require("./routes");
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middleware();
+    this.route();
+  }
+
+  middleware() {
+    this.server.use(express.json());
+  }
+  route() {
+    this.server.use(routes);
+  }
+}
+
+module.exports = new App().server;
