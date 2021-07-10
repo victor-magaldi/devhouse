733d01fdb00827b7f7191c7758e8292892e091ce
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-10 19:34:05 -0300

    instalando mongoose e concetando

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
 import routes from "./routes";
 
 class App {
   constructor() {
     this.server = express();
 
+    mongoose.connect(
+      "mongodb+srv://devhouse:devhouse12345678@devhouse.3gauw.mongodb.net/devhouse?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     this.middleware();
     this.route();
   }
