9701dd1d306920ddcf81101a51d6c897384c7609
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-13 21:10:11 -0300

    utilizando dotenv

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import routes from "./routes";
 
+require("dotenv").config();
 class App {
   constructor() {
     this.server = express();
 
     mongoose.connect(
-      `mongodb+srv://devhouse:senhaaaaaaa@devhouse.3gauw.mongodb.net/devhouse?retryWrites=true&w=majority`,
+      `mongodb+srv://devhouse:${process.env.PASSWORD_MONGO_DEVHOUSE_BD}@devhouse.3gauw.mongodb.net/devhouse?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
