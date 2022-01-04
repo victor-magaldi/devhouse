7a3e37ccf5a64d3f64d631819c91d32ce00b15b4
victor de souza magaldi <victmagaldi@hotmail.com>
2022-01-04 20:33:38 -0300

    validation user not authorized

--- backend/src/controllers/HouseController.js
+++ backend/src/controllers/HouseController.js
@@ -7,6 +7,7 @@ update : atualizar alguma sessão
 destroy: deletar uma sessão
 */
 import House from "../models/House";
+import User from "../models/User";
 
 class HouseController {
   constructor() {}
@@ -41,7 +42,14 @@ class HouseController {
     const { description, price, location, status } = req.body;
     const { user_id } = req.headers;
 
-    const updateHouse = await House.updateOne(
+    const user = await User.findById(user_id);
+    const houses = await House.findById(house_id);
+
+    if (String(user._id) !== String(houses.user)) {
+      return res.status(401).json({ error: "user not authorized." });
+    }
+
+    await House.updateOne(
       { _id: house_id },
       {
         user: user_id,
@@ -52,7 +60,7 @@ class HouseController {
         status,
       }
     );
-    return res.json(updateHouse);
+    return res.send();
   }
 }
 
