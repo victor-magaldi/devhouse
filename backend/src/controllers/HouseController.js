bbb285e4c5d642f1c017d2934cc4ec908955356d
victor de souza magaldi <victmagaldi@hotmail.com>
2022-01-04 20:19:18 -0300

    update house

--- backend/src/controllers/HouseController.js
+++ backend/src/controllers/HouseController.js
@@ -10,12 +10,14 @@ import House from "../models/House";
 
 class HouseController {
   constructor() {}
+
   async index(req, res) {
     const { status } = req.query;
 
     const houses = await House.find({ status });
     return res.json(houses);
   }
+
   async store(req, res) {
     const { filename } = req.file;
     const { description, price, location, status } = req.body;
@@ -31,6 +33,26 @@ class HouseController {
     });
 
     return res.json(house);
+  }
+
+  async update(req, res) {
+    const { filename } = req.file;
+    const { house_id } = req.params;
+    const { description, price, location, status } = req.body;
+    const { user_id } = req.headers;
+
+    const updateHouse = await House.updateOne(
+      { _id: house_id },
+      {
+        user: user_id,
+        thumbnail: filename,
+        description,
+        price,
+        location,
+        status,
+      }
+    );
+    return res.json(updateHouse);
   }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,5 +10,10 @@ const upload = multer(uploadConfig);
 routes.post("/session", SessionController.store);
 routes.post("/houses", upload.single("thumbnail"), HouseController.store);
 routes.get("/houses", HouseController.index);
+routes.put(
+  "/houses/:house_id",
+  upload.single("thumbnail"),
+  HouseController.update
+);
 
 export default routes;
