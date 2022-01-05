ec2207e99b8faca845da77010b8487922f6f1656
victor de souza magaldi <victmagaldi@hotmail.com>
2022-01-05 20:56:00 -0300

    show all houses

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ import multer from "multer";
 import uploadConfig from "./config/upload";
 import SessionController from "./controllers/SessionController";
 import HouseController from "./controllers/HouseController";
+import DashboardController from "./controllers/DashboardController";
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -16,5 +17,7 @@ routes.put(
   HouseController.update
 );
 routes.delete("/houses/:house_id", HouseController.destroy);
+
+routes.get("/dashboard", DashboardController.show);
 
 export default routes;
