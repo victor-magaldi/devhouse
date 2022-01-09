757c80c212025f419d6170f0cf653a4476fee34e
victor de souza magaldi <victmagaldi@hotmail.com>
2022-01-08 22:08:42 -0300

    validate

--- backend/src/controllers/ReserveController.js
+++ backend/src/controllers/ReserveController.js
@@ -6,12 +6,25 @@ class ReserveController {
     const { house_id } = req.params;
     const { date } = req.body;
 
+    const house = await House.findById(house_id);
+    if (!house) {
+      res.status(400).json({ error: "Esta casa não existe" });
+    }
+
+    if (house?.status !== true) {
+      res.status(400).json({ error: "Solicitação indisponível" });
+    }
+
+    const user = await User.findById(user_id);
+    if (String(user?._id) === String(house?.user)) {
+      res.status(401).json({ error: "Reserva não permitida" });
+    }
     const reserve = await Reserve.create({
       user: user_id,
       house: house_id,
       date,
     });
-    //consegue popular mais o Reserve com referencias dos outros models
+    //consegue popular mais o Model Reserve com referencias dos outros models
     await reserve.populate("house").populate("user").execPopulate();
 
     return res.json(reserve);
